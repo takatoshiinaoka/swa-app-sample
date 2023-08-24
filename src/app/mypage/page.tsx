ab2289cfd0b10149b6cabba3c4c367448d790634
taatoshiinaoka <t-inaoka@alterbooth.com>
2023-08-24 15:37:42 +0900

    [WIP] 認証テスト

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function MyPage() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    fetch("/.auth/me")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUser(data.clientPrincipal.userDetails);
+      });
+  }, []);
+
   return (
     <main>
-      <h1>こんにちは、Nameさん!</h1>
+      <h1>こんにちは、{user} さん!</h1>
+      <a href="/.auth/logout?post_logout_redirect_uri=/">ログアウト</a>
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>ログイン</h1>
+      <div>
+        <a
+          href="/.auth/login/github?post_login_redirect_uri=/mypage"
+          className={styles.link}
+        >
+          Login
+        </a>
+        <a href="/mypage" className={styles.link}>
+          mypage
+        </a>
+      </div>
     </main>
   );
 }
