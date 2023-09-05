0f31fa1ed3faed269cc1f96712788a8e2304d492
taatoshiinaoka <t-inaoka@alterbooth.com>
2023-09-05 16:53:49 +0900

    [add] コマンドの追加

---
+++ docs/command.sh
@@ -0,0 +1,14 @@
+# Reactプロジェクト作成
+npm create vite@latest react-app -- --template react
+
+# Azure Function Core Toolsのインストール​
+npm i -g azure-functions-core-tools@4 --unsafe-perm true
+
+# Funcitonsプロジェクト作成​
+func init <PROJECT_FOLDER>
+
+# Funcitons作成​
+func new
+
+# Functions起動
+func start
